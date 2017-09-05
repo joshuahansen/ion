424b3c5461e8ade7cba8e26008f248cbb097fc00
Josh <joshua.hansen188@gmail.com>
2017-09-05 22:00:24 +1000

    working without jquery

--- ion-min/javascript/script.js
+++ ion-min/javascript/script.js
@@ -49,11 +49,11 @@ function loadContent(file, activeMenuId, loadData) {
 	{
     		if (this.readyState == 4 && this.status == 200)
 		{
+			document.getElementById("display").innerHTML = this.responseText;
 			if(loadData)
 			{
 				loadData();
 			}
-			document.getElementById("display").innerHTML = this.responseText;
   	  	}
   	};
 	xhttp.open("GET", file, true);
@@ -72,7 +72,8 @@ function loadContent(file, activeMenuId, loadData) {
 	}
 	if(activeMenuId.localeCompare("info") == 0)
 	{
-		updateCurrentDials();
+//		updateCurrentDials();
+		updateCurrentValues();
 	}
 }
 
@@ -86,38 +87,62 @@ function closeConfig() {
 
 /**********************************************************************************/
 /*Info JS*/
+function loadJSON(file, callback)
+{
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() 
+	{
+    		if (this.readyState == 4 && this.status == 200)
+		{
+			callback(this.responseText);
+  	  	}
+  	};
+	xhttp.open("GET", file, true);
+	xhttp.send();	
+}
+function sendJSON(url)
+{
+	var xhttp = new XMLHttpRequest();
+
+	xhttp.open("GET",url,true);
+	xhttp.send();
+}
 function loadSensors()
 {
 //	dataInterval = setInterval(updateCurrentDials, 15000);
-	updateCurrentDials();
+//	updateCurrentDials();
+	updateCurrentValues();
 }
 function updateCurrentValues()
 {
-	$.getJSON("current", function(data) { 
+	loadJSON("current", function(jsonData) { 
+		var data = JSON.parse(jsonData);
 		for(var i = 0; i < data.sensors.length; i++)
 		{
 			//Set value to empty string if no sensor is attached
 			if(data.sensors[i].type == 0)
 			{
-				$("#s" + data.sensors[i].port + "value").text("");
+				document.getElementById("s" + data.sensors[i].port + "value").innerHTML = data.sensors[i].stringValue;
 			}
 			//Set all port Values
-			$("#s" + data.sensors[i].port + "value").text(data.sensors[i].stringValue);
+			document.getElementById("s" + data.sensors[i].port + "value").innerHTML = data.sensors[i].stringValue;
+			document.getElementById("s" + data.sensors[i].port + "value").style.fontSize = "50px";
+			document.getElementById("port" + data.sensors[i].port).style.visibility = "hidden";
 			//Set all port types
-			$("#s"+ data.sensors[i].port + "type").text(sensorTypes[data.sensors[i].type]);
+			document.getElementById("s" + data.sensors[i].port + "type").innerHTML = sensorTypes[data.sensors[i].type];
 			//Set all port names
 			if(data.sensors[i].name.localeCompare("") != 0)
 			{
-				$("#s" + data.sensors[i].port + "label").text(data.sensors[i].name);
-				
+				document.getElementById("s" + data.sensors[i].port + "label").innerHTML = data.sensors[i].name;
 			}
 		}
 	});
 }
 function updateCurrentDials()
 {
-	$.getJSON("current", function(data) { 
+	loadJSON("current", function(jsonData) { 
 		success:
+		var data = JSON.parse(jsonData);
 		for(var i = 0; i < data.sensors.length; i++)
 		{
 			//Set value to empty string if no sensor is attached
@@ -218,6 +243,137 @@ function updateCurrentDials()
 		}
 	});
 }
+function setThres(portNum) {
+	loadJSON("current", function(jsonData) {
+		success: {
+			var data = JSON.parse(jsonData);
+			document.getElementById("portNum").value = portNum + 1;
+			if(data.sensors[portNum].type == 0) 
+			{
+				//do nothing
+			}
+			else if(data.sensors[portNum].type == 4)
+			{
+				//set threshold for door and dry contact sensors
+				document.getElementById("maxThreshold").max = 1;
+				document.getElementById("maxThreshMax").innerHTML = "Closed";
+				document.getElementById("maxThreshMin").innerHTML = "Open";
+
+				document.getElementById("portName").value = data.sensors[portNum].name;
+				//set max threshold for port using current values
+				document.getElementById("maxValue").value = data.sensors[portNum].threshMax;
+				document.getElementById("maxThreshold").value = data.sensors[portNum].threshMax;
+				document.getElementById("minThreshRange").style.display = "none";
+				document.getElementById("setThresh").value = 4;
+
+				document.getElementById("sensorModal").style.display = "block";
+			}
+			else if(data.sensors[portNum].type == 6)
+			{
+				//set thresh for contact sensors
+				document.getElementById("maxThreshold").max = 1;
+				document.getElementById("maxThreshMax").innerHTML = "No Contact";
+				document.getElementById("maxThreshMin").innerHTML = "Contact";
+
+				document.getElementById("portName").value = data.sensors[portNum].name;
+				//set max threshold for port using current values
+				document.getElementById("maxValue").value = data.sensors[portNum].threshMax;
+				document.getElementById("maxThreshold").value = data.sensors[portNum].threshMax;
+				document.getElementById("minThreshRange").style.display = "none";
+				document.getElementById("setThresh").value = 6;
+
+				document.getElementById("sensorModal").style.display = "block";
+			}
+			else if(data.sensors[portNum].type == 8)
+			{
+				//set thresh for leak rope
+				document.getElementById("maxThreshold").max = 1;
+				document.getElementById("maxThreshMax").innerHTML = "Leak";
+				document.getElementById("maxThreshMin").innerHTML = "No Leak";
+
+				document.getElementById("portName").value = data.sensors[portNum].name;
+				//set max threshold for port using current values
+				document.getElementById("maxValue").value = data.sensors[portNum].threshMax;
+				document.getElementById("maxThreshold").value = data.sensors[portNum].threshMax;
+				document.getElementById("minThreshRange").style.display = "none";
+				document.getElementById("setThresh").value = 8;
+
+				document.getElementById("sensorModal").style.display = "block";
+			}
+			else
+			{
+				//set sensors values for teperature sensors
+				document.getElementById("minThreshold").max = 100;
+				document.getElementById("minThreshMax").innerHTML = 100;
+	
+				document.getElementById("maxThreshold").max = 100;
+				document.getElementById("maxThreshMax").innerHTML = 100;
+				document.getElementById("maxThreshMin").innerHTML = 0;
+
+				document.getElementById("portName").value = data.sensors[portNum].name;
+				//set min hreshold for port using current values
+				document.getElementById("minThreshold").value = data.sensors[portNum].threshMin;
+				document.getElementById("minValue").value = data.sensors[portNum].threshMin;
+				//set max threshold for port using current values
+				document.getElementById("maxValue").value = data.sensors[portNum].threshMax;
+				document.getElementById("maxThreshold").value = data.sensors[portNum].threshMax;
+				document.getElementById("setThresh").value = 1;
+				
+				document.getElementById("minThreshRange").style.display = "block";
+				document.getElementById("sensorModal").style.display = "block";
+			}
+
+		}
+	});
+}
+function updateThresh()
+{
+	var setThreshValue = document.getElementById("setThresh").value;
+	var name = document.getElementById("portName").value;
+	var portNum = document.getElementById("portNum").value;
+	
+	if(setThreshValue == 4 || setThreshValue == 6 || setThreshValue == 8)
+	{
+		var max = document.getElementById("maxThreshold").value;
+		sendJSON("/sensors/port" + portNum + "/?name=" + name + "&maxThresh=" + max);
+		document.getElementById("sensorModal").style.display = "none";
+		return false;
+	}
+	else 
+	{
+		var max = document.getElementById("maxThreshold").value;
+		var min = document.getElementById("minThreshold").value;
+		sendJSON("/sensors/port" + portNum + "/?name=" + name + "&maxThresh=" + max + "&minThresh=" + min);
+		document.getElementById("sensorModal").style.display = "none";
+		return false;
+	}
+}
+function showMinValue(value)
+{
+	document.getElementById("minValue").value = value;
+}
+function changeMinRange(value)
+{
+	document.getElementById("minThreshold").value = value;
+}
+function showMaxValue(value)
+{
+	document.getElementById("maxValue").value = value;
+}
+function changeMaxRange(value)
+{
+	document.getElementById("maxThreshold").value = value;
+}
+
+function closeModal() {
+	document.getElementById("sensorModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+	if(event.target == document.getElementById("sensorModal")) {
+		document.getElementById("sensorModal").style.display = "none";
+	}
+}
 /**********************************************************************************/
 /*Alerts JS*/
 function addRow(stats, descr, rowNum) {
@@ -232,7 +388,8 @@ function addRow(stats, descr, rowNum) {
 
 function loadAlerts() {
 	
-	$.getJSON("current", function(data) {
+	loadJSON("current", function(jsonData) {
+		var data = JSON.parse(jsonData);
 		var counter = 1;
 		for(var i = 0; i < data.sensors.length; i++)
 		{
@@ -264,8 +421,9 @@ function addAlert(port, thresh, counter)
 /*******************************************************************************/
 function loadEmails()
 {
-	$.getJSON("emails", function(data) {
+	loadJSON("emails", function(jsonData) {
 		success: {
+			var data = JSON.parse(jsonData);
 			//load email 1
 			document.getElementById("email1").value = data[0].email;
 			if(data[0].disable == 0)
@@ -287,11 +445,51 @@ function loadEmails()
 		}
 	});
 }
+function updateEmails()
+{
+	//email 1
+	var email1 = document.getElementById("email1").value;
+	var disable1 = document.getElementById("disableEmail1").checked;
+	if(disable1 == true)
+	{
+		disable1 = 0;
+	}
+	else
+	{
+		disable1 = 1;
+	}
+	//email 2
+	var email2 = document.getElementById("email2").value;
+	var disable2 = document.getElementById("disableEmail2").checked;
+	if(disable2 == true)
+	{
+		disable2 = 0;
+	}
+	else
+	{
+		disable2 = 1;
+	}
+	//email 3
+	var email3 = document.getElementById("email3").value;
+	var disable3 = document.getElementById("disableEmail3").checked;
+	if(disable3 == true)
+	{
+		disable3 = 0;
+	}
+	else
+	{
+		disable3 = 1;
+	}
+	
+	loadJSON("/emails/?email1=" + email1 + "&disable1=" + disable1 + "&email2=" + email2 + "&disable2=" + disable2 + "&email3=" + email3 + "&disable3=" + disable3);
+	return false;
+}
 /**********************************************************************************/
 /*Network Settings*/
 function loadNetwork()
 {
-	$.getJSON("network", function(data) {
+	loadJSON("network", function(jsonData) {
+		var data = JSON.parse(jsonData);
 		document.getElementById("ipInput").value = data.ip;
 		document.getElementById("subnetInput").value = data.subnet;
 		document.getElementById("gatewayInput").value = data.gateway;
@@ -299,6 +497,24 @@ function loadNetwork()
 		document.getElementById("sdnsInput").value = data.sdns;
 	});
 }
+function updateNetwork()
+{
+		var ip = document.getElementById("ipInput").value;
+		var subnet = document.getElementById("subnetInput").value;
+		var gateway = document.getElementById("gatewayInput").value;
+		var pdns = document.getElementById("pdnsInput").value;
+		var sdns = document.getElementById("sdnsInput").value;
+
+		sendJSON("/network?ip=" + ip + "&subnet=" + subnet + "&gateway=" + gateway + "&pdns" + pdns + "&sdns=" + sdns);
+	return false;
+}
+/*************************************/
+/*Reboot*/
+function reboot()
+{
+	sendJSON("reboot?true");
+}
+
 
 /**********************************************************************************/
 /*System Time Settings*/
@@ -317,7 +533,8 @@ function systemTime()
 }
 function loadNTP()
 {
-	$.getJSON("ntp", function(data) {
+	loadJSON("ntp", function(jsonData) {
+		var data = JSON.parse(jsonData);
 		document.getElementById("NTPServer").value = data.NTP;
 		document.getElementById("timeZone").value = "GMT" + data.timeZone;
 		document.getElementById("daylightSavings").value = data.daylight;
@@ -339,11 +556,69 @@ function loadTime()
 	loadNTP();
 	dataInterval = setInterval(systemTime, 1000);
 }
+function updateDate()
+{
+	var radioValue;
+	var radio = document.getElementsByName('systemTime');
+	for(var i = 0; i < radio.length; ++i)
+	{
+		if(radio[i].checked)
+		{
+			radioValue = radio[i].value;
+		}
+	}
+	if(radioValue.localeCompare("ntp") == 0)
+	{	
+		//SEND UPDATE FEQUENCY, SERVER IP, TIME ZONE, AND DAYLIGHT SAVINGS
+		var NTP = document.getElementById("NTPServer").value;
+		var timeZone = document.getElementById("timeZone").value;
+		var dayLight = document.getElementById("daylightSavings").value;
+		if(timeZone.localeCompare("GMT") == 0)
+		{
+			timeZone = "0:00";
+		}
+		else
+		{
+			timeZone = timeZone.replace("GMT", "");
+		}
+
+		
+		sendJSON("/systemTime?NTP=" + NTP + "&timeZone=" + timeZone + "&daylight=" + dayLight);
+		return false;
+	}
+	else if(radioValue.localeCompare("sysTime") == 0)
+	{
+		//SEND SYSTEM TIME IN UNIX TIMESTAMP
+		var date = new Date();
+		var unixtime = date.getTime();
+		sendJSON("/systemTime?unix=" + unixtime);
+		return false;
+	}
+	else if(radioValue.localeCompare("manual") == 0)
+	{
+		//CONVERT MANUAL INPUT TO UNIX TIMESTAMP
+		var manDate = new Date(document.getElementById("manualDate").value);
+		var day = manDate.getDate();
+		var month = manDate.getMonth();
+		var year = manDate.getYear();
+		var time = document.getElementById("manualTime").value;
+		var unixtime = Date.parse("15 Aug, 2017 16:00:35")/1000;
+		sendJSON("/systemTime?unix=" + unixtime);
+		return false;
+	}
+}
+/**************************************/
+/*FIRMWARE*/
+function upgradeFirmware()
+{
+
+}
 /**********************************************************************************/
 /*SNMP Settings*/
 function loadSNMP()
 {
-	$.getJSON("snmp", function(data) {
+	loadJSON("snmp", function(jsonData) {
+		var data = JSON.parse(jsonData)
 		document.getElementById("snInput").value = data.sysName;
 		document.getElementById("scInput").value = data.sysContact;
 		document.getElementById("slInput").value = data.sysLocation;
@@ -352,6 +627,21 @@ function loadSNMP()
 		document.getElementById("snmpv3id").value = data.SNMPv3ID;
 		document.getElementById("snmpv3input").value = data.SNMPv3;
 	});
+}
+function updateSNMP()
+{
+		var sysName = document.getElementById("snInput").value;
+		var sysContact = document.getElementById("scInput").value;
+		var sysLocation = document.getElementById("slInput").value;
+		var netAgent = document.getElementById("netagentport").value;
+		var trapPort = document.getElementById("trapport").value;
+		var SNMPv3ID = document.getElementById("snmpv3id").value;
+		var SNMPv3 = document.getElementById("snmpv3input").value;
+
+		sendJSON("/snmp?sysName=" + sysName + "&sysContact=" + sysContact + 
+			"&sysLocation=" + sysLocation + "&netAgent" + netAgent + 
+			"&trapPort=" + trapPort + "&SNMPv3ID" + SNMPv3ID + "&SNMPv3" + SNMPv3);
+	return false;
 }
 
 /**********************************************************************************/
@@ -371,9 +661,9 @@ function loadHistory()
 	var datasetVolt = [];
 	var datasets = [datasetTemp, datasetSmoke, datasetTH, datasetDoor, datasetVibr, datasetDry, datasetVolt];
 
-	$.getJSON("sensorhistory", function(data) {
+	loadJSON("sensorhistory", function(jsonData) {
 		success: {
-			
+			var data = JSON.parse(jsonData);
 			for(var i = 0; i < data.length; i++)
 			{
 				var type = data[i].type;
@@ -539,7 +829,6 @@ function lineChart(canvas, chart, attributes)
 	}
 	/*add port labels*/
 	var labelYPos = 15;
-//	var rangeOfValues = attributes.data.datasets.length;
 	newPoint = 0;
 	lineLength = spacing * 0.7;
 	for(var i = 0; i < rangeOfValues; ++i)
@@ -561,7 +850,6 @@ function lineChart(canvas, chart, attributes)
 	}
 
 	/*add data*/
-//	var rangeOfValues = attributes.data.datasets.length;
 	for(var i = 0; i < rangeOfValues; ++i)
 	{
 		newPoint = 0;
@@ -586,4 +874,27 @@ function lineChart(canvas, chart, attributes)
 		}
 	}
 
+}
+/***************************************/
+/*ADMIN*/
+function updatePass()
+{
+	var currentPass = document.getElementById("currentPassInput").value;
+	var newPass = document.getElementById("newPassInput").value;
+	var confPass = document.getElementById("confirmPassInput").value;
+	
+	if(currentPass.length < 4 || newPass.length < 4 || confPass.length < 4)
+	{
+		console.log("ERROR");
+		document.getElementById("errormsg").innerHTML = "Passwords must be at least 4 characters long"; 
+	}
+	else if(newPass.localeCompare(confPass) != 0)
+	{
+		console.log("PASS DONT MATCH");
+		document.getElementById("errormsg").innerHTML = "Passwords do not match";
+	}
+	else 
+	{
+		loadJSON("/admin?currentPass=" + currentPass + "&newPass=" + newPass + "&confPass=" + confPass);
+	}
 }
